2aae1f70ae7c36586f60480cf6c7ff44d5eaa82c
mewisliving <milkysolomon123@gmail.com>
2026-06-30 16:38:51 +0300

    checking if the repository is connected

---
+++ checking.py
@@ -0,0 +1,5 @@
+#checking if it's working, and the hackatime toooo bla bla bla bkaaaaaa. I'm making an app. my first actually.
+ #i'm new to this and dont know how it worksss
+#it would take a lot of time but i'm certain it's going one of the vital tool 
+#i'm writing all this so that the timer becomes 1 min. I dont think it's workinggggg
+# ooo 2min.
